575e674b59b83f6a297229a61be5c9dab449ce1d
MuratOzte <mo2553481@gmail.com>
2024-02-22 02:10:55 +0300

    Add technologies, overview, live website, example usernames and passwords, and images to README.md. Update package-lock.json and package.json to include @vercel/analytics. Add Analytics component to layout.tsx.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+# Technologies
 ![Tecnologies](https://skillicons.dev/icons?i=ts,next,tailwind,prisma,mongodb,firebase)
+
+# Overwiev
+A messaging application similar to WhatsApp, developed without using technologies like Socket.io or Pusher.
+
+# Live Website
+To see the project live, you can click [here](https://whatsapp-clone-iota-olive.vercel.app/messages).
+
+## Example usernames and passwords for testing
+
+| Username  | Password |
+| ------------- |:-------------:|
+| admin      | admin     |
+| test      | test     |
+
+## Images
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 import ToasterContext from './context/ToasterContext';
 import AuthContext from './context/AuthContext';
@@ -25,6 +26,7 @@ export default function RootLayout({
                     <Providers>
                         <ToasterContext />
                         {children}
+                        <Analytics />
                     </Providers>
                 </AuthContext>
             </body>
