3289753beed7c81f707c5f0e9670ae20b2e127ec
MuratOzte <mo2553481@gmail.com>
2024-10-13 20:39:53 +0300

    time bug fixed

--- src/components/messages/Message.tsx
+++ src/components/messages/Message.tsx
@@ -11,7 +11,7 @@ const Message: React.FC<MessageProps> = ({
     date,
     text,
 }) => {
-    const time = date.split(' at ')[1].split(':').slice(0, 2).join('.');
+    // const time = date.split(' at ')[1].split(':').slice(0, 2).join('.');
 
     return (
         <div
@@ -23,7 +23,7 @@ const Message: React.FC<MessageProps> = ({
                 <div className="bg-search-nav w-fit px-3 py-1 text-gray-400 rounded-lg my-2 flex flex-col items-start">
                     <p className="text-sm break-words max-w-48 md:max-w-72">{text}</p>
                     <div className="flex">
-                        <p className="text-[8px] mt-1">{time}</p>
+                        <p className="text-[8px] mt-1">{'deneme'}</p>
                     </div>
                 </div>
             )}
@@ -31,7 +31,7 @@ const Message: React.FC<MessageProps> = ({
                 <div className="bg-phone-mymessage w-fit px-3 py-1 text-gray-300 rounded-lg my-1 flex flex-col items-end ">
                     <p className="text-sm break-words max-w-48 md:max-w-72">{text}</p>
                     <div className="flex">
-                        <p className="text-[8px] mt-1">{time}</p>
+                        <p className="text-[8px] mt-1">{'deneme'}</p>
                     </div>
                 </div>
             )}
